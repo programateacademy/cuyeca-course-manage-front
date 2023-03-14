d68d8d9ea53e6e4fc2053114adc04d7dd430e652
Carmenvic98 <carmenv.rios.cr@gmail.com>
2023-03-14 14:20:07 -0500

    continuacion de la elaboración de tarjetas

--- src/components/card.js
+++ src/components/card.js
@@ -1,15 +1,14 @@
 import React from 'react'
 
-import img1 from '../assets/img1.png'
 
-function card() {
+function card({title, imageSource}) {
   return (
     <div className="card text-center">
         <div className="card-body">
-            <h4 className="card-title">Nombre Modulo</h4>
-            <img src={img1} alt="imagen"/>
+            <h4 className="card-title">{title}</h4>
+            <img src={imageSource} alt="imagen"/>
             <h5 className="card-text">Descripción</h5>
-            <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <a href="#!" className="btn btn-outline-secondary border-0"><p className="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p></a>
         </div>
     </div>
   )
--- src/components/cards.js
+++ src/components/cards.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import Card from "./card";
 
-function cards() {
+import img1 from '../assets/img1.png'
+import img2 from '../assets/img2.png'
+import img3 from '../assets/img3.png'
+
+const cards = [
+    {
+        id: 1,
+        title: "Leccion 1",
+        image: img1
+    },
+    {
+        id: 2,
+        title: "Leccion 2",
+        image: img2
+    },
+    {
+        id: 3,
+        title: "Leccion 3",
+        image: img3
+    }
+]
+
+function Cards() {
+    console.log(cards)
   return (
     <div className="container d-flex justify-content-center align-items-center h-100">
         <div className="row">
-            <div className="col-md-4">
-                <Card/>
-            </div>
-            <div className="col-md-4">
-                <Card/>
-            </div>
-            <div className="col-md-4">
-                <Card/>
-            </div>
+            {
+                cards.map(card => (
+                    <div className="col-md-4" key={card.id}>
+                        <Card title={card.title} imageSource={card.image}/>
+                    </div>
+                ))
+            }
         </div>
     </div>
   )
 }
 
-export default cards
+export default Cards
