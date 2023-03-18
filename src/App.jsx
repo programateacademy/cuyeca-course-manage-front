f8ab8b92b9d61d2cc2978b1c0fc3d7fdfafc1b1f
carolinaviasus <carolinaviasus@outlook.com>
2023-03-17 23:14:41 -0500

    creation of the form with styles and responsive

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ const App = () => {
 
       <Router>
         <Header/>
-
+        {message}
         <Routes>
           <Route path='/' element={<Cards/>}>  </Route>
           <Route path='/admin' element={<Login/>}>  </Route>
