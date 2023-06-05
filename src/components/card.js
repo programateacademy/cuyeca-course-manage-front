cdd07ea0bdc2359c1ed5ae9acc0e0e1538514b3a
Diego <yennivanessadelgado@gmail.com>
2023-06-05 10:44:03 -0500

    banner actualizado

--- src/components/card.js
+++ src/components/card.js
@@ -13,11 +13,11 @@ function card({title, imageSource, text, url,video,resources}) {
               <img src={imageSource} alt="imagen" className="card-img" />
             </div>
             <h5 className="card-text">Descripción</h5>
-            <a href={url} className="btn border-0"><p className="card-text1">
+           <p className="card-text1">
               {
                 text ? text : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
               }
-              </p></a>
+              </p>
               <a href={video} target="_blank" rel="noopener noreferrer" className="btn btn-outline-danger">
               <i className="bi bi-youtube p-3"></i>
               </a>
